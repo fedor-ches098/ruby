cef026c0b1555c82861fe3355cc660ace25bc933
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-01-24 17:20:08 +0300

    edit ideal_weight.rb

--- first_lesson/ideal_weight.rb
+++ first_lesson/ideal_weight.rb
@@ -4,6 +4,5 @@ puts "Введите Ваш рост"
 height = gets.to_i
 ideal_weight = (height - 110) * 1.15
 puts "#{name.capitalize}! Ваш идеальный вес: #{ideal_weight}"
-if ideal_weight < 0
-  puts "Ваш вес уже оптимальный"
-end
+puts "Ваш вес уже оптимальный" if ideal_weight < 0
+
