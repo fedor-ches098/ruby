1d659657361978d0d5190cade80d4d60a9f8d084
Fedor Chesnokov <fedor@chesnokov-ror-course.cloud.lab>
2024-01-29 11:58:50 +0300

    edit exercise_1.rb

--- third_lesson/exercise_1.rb
+++ third_lesson/exercise_1.rb
@@ -1,83 +1,48 @@
 class Station
   attr_accessor :trains, :name
+
   def initialize(name)
     @name = name
     @trains = []
   end
 
-  def show_trains
-    return @trains if check_trains == false
-    
-    @trains.each {|train| puts "Train number: #{train.number} now in station: #{self.name}"}
-  end
-
-  def show_trains_by_type(type)
-    return @trains if check_trains == false
-
-    if (type != "cargo") && (type != "passenger")
-      puts "Error! Type must be 'cargo' or 'passenger'"
-      return @trains
-    end
-
-    @trains.select {|train| puts "Train number: #{train.number} with type #{train.type} now in station: #{self.name}" if train.type == type}
+  def trains_by_type(type)
+    trains.select {|train| train.type == type}.size
   end
 
   def take_train(train)
     @trains << train
-    puts "Take train: #{train.number} to station: #{self.name}"
   end
 
   def send_train(train)
-    return @trains if check_trains == false
-
     @trains.delete(train)
-    puts "Send train: #{train.number} from station: #{self.name}"
-  end
-
-  private 
-
-  def check_trains
-    if @trains.empty?
-      puts "Error! No trains in station: #{self.name}"
-      return false
-    end
   end
 end
 
 class Route
   attr_accessor :stations
+
   def initialize(from, to)
     @stations = [from, to]
   end
 
-  def add_intermediate_station(station)
+  def add_station(station)
     @stations.insert(-2, station)
-    puts "Add intermediate station: #{station}. Stations: #{@stations}"
   end
 
-  def remove_intermediate_station(station)
-    unless @stations.include? station
-      puts "Error! Can not find intermediate station: '#{station}' in stations array"
-      return @stations
-    end
-        
-    if (station == @stations[0]) || (station == @stations[-1])
-      puts "Error! Can not remove first or last station"
-      return @stations
-    else
-      @stations.delete(station)
-      puts "Remove intermediate station: #{station}. Stations: #{@stations}"
-    end
+  def remove_station(station)
+    @stations.delete(station)
   end
 
   def show_stations
-    @stations.each {|station| puts "Station: #{station} in route"}
+    puts stations
   end
 end
 
 class Train
-  attr_accessor :speed, :car_count, :route, :current_station
+  attr_accessor :speed, :car_count, :route
   attr_reader   :number, :type
+  
   def initialize(number, type, car_count)
     @number = number
     @type = type
@@ -87,101 +52,77 @@ class Train
 
   def stop
     @speed = 0
-    puts "Train: #{self.number} is stopped. Speed: #{@speed}"
   end
 
-  def change_car_count(action)
-    if (@car_count == 0) && (action == "remove")
-      puts "Nothing to remove!" 
-      return @car_count
-    end
+  def remove_car
+    @car_count -= 1 if @speed == 0
+  end
 
-    unless @speed == 0
-      puts "Error! Train: #{self.number} is going"
-      return @car_count
-    end
-
-    if action == "remove"
-      @car_count -= 1 
-      puts "Remove car for train: #{self.number}! Count: #{@car_count}"
-    elsif action == "add"   
-      @car_count += 1
-      puts "Add car for train: #{self.number}! Count: #{@car_count}"
-    else    
-      puts "Error! Action must be 'add' or 'remove'"
-    end
+  def add_car
+    @car_count += 1 if @speed == 0
   end
 
   def take_route(route)
     self.route = route
-    puts "Train: #{self.number} take route: #{route.stations}"
-    self.current_station = route.stations[0]
-    puts "Train's: #{self.number} current station: #{current_station}"
+    @current_station_index = 0
+
+    current_station
   end
 
-  def move_train(direction)
-    return false if check_route_assign == false
-
-    if (self.current_station == self.route.stations[0]) && (direction == "back")
-      puts "Error! Train: #{self.number} in first route station"
-      return self.current_station
-    end
-
-    if direction == "forward"
-      self.current_station = route.stations[route.stations.find_index(self.current_station) + 1]
-      puts "Train: #{self.number} go forward. Current station: #{self.current_station}" 
-    end
-
-    if direction == "back"
-      self.current_station = route.stations[route.stations.find_index(self.current_station) - 1]
-      puts "Train: #{self.number} go back. Current station: #{self.current_station}"
-    end
+  def go_next_station
+    @current_station_index += 1 if next_station
   end
 
-  def show_train_route_info
-    return false if check_route_assign == false
+  def go_previous_station
+    @current_station_index -= 1 if previous_station
+  end 
 
-    puts "Previous station: #{route.stations[route.stations.find_index(self.current_station) - 1]} for train: #{self.number}"
-    puts "Current station: #{self.current_station} for train: #{self.number}"
-    puts "Next station: #{route.stations[route.stations.find_index(self.current_station) + 1]} for train: #{self.number}"
+  def current_station
+    route.stations[@current_station_index] unless route.nil?
   end
 
-  private 
-
-  def check_route_assign
-    if self.route.nil?
-      puts "Error! Route not assign fro train: #{self.number}"
-      return false
-    end
+  def next_station
+    route.stations[@current_station_index + 1] unless route.nil?
   end
+
+  def previous_station
+    route.stations[@current_station_index - 1] unless route.nil?
+  end 
 end
 
 puts "-----------------------------------------"
 route = Route.new("Pavshino", "Dmitrovskaya")
-route.add_intermediate_station("Tyshinskaya")
-route.add_intermediate_station("Shukinskaya")
-route.remove_intermediate_station("Shukinskaya")
+route.add_station("Tyshinskaya")
+route.add_station("Shukinskaya")
+route.remove_station("Shukinskaya")
 route.show_stations
 puts "-----------------------------------------"
 train_1 = Train.new("1490", "cargo", 20)
 train_2 = Train.new("1500", "passenger", 10)
 train_3 = Train.new("1495", "cargo", 15)
 train_1.take_route(route)
-train_1.move_train("forward")
-train_1.move_train("forward")
-train_1.move_train("back")
-train_1.show_train_route_info
-train_2.move_train("forward")
-train_2.show_train_route_info
-train_3.change_car_count("add")
+puts train_1.current_station
+train_1.go_next_station
+train_1.go_next_station
+train_1.go_previous_station
+puts train_1.previous_station
+puts train_1.current_station
+puts train_1.next_station
+train_2.go_next_station
+puts train_2.current_station
+train_2.speed = 120
+train_2.add_car
+puts train_2.car_count
+train_3.add_car
+puts train_3.car_count
 puts "-----------------------------------------"
 station_1 = Station.new("Pavshino")
 station_2 = Station.new("Depo")
-station_2.show_trains_by_type("cargo")
+station_2.trains_by_type("cargo")
 station_1.take_train(train_1)
 station_1.take_train(train_2)
 station_1.take_train(train_3)
-station_1.show_trains
-station_1.show_trains_by_type("cargo")
+station_1.trains
+puts station_1.trains_by_type("cargo")
 station_1.send_train(train_1)
-station_1.show_trains
+station_1.trains
